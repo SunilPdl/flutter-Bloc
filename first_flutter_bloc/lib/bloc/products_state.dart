44fe0e6d5614c7bdc20640e5afba67528b680f84
SunilPdl <mrsunilpaudel@gmail.com>
2024-01-16 14:50:56 +0545

    update : directory name

--- first_flutter_bloc/lib/bloc/products_state.dart
+++ first_flutter_bloc/lib/bloc/products_state.dart
@@ -1,6 +1,17 @@
 part of 'products_bloc.dart';
 
 @immutable
-sealed class ProductsState {}
+sealed class ProductsState extends Equatable {
+  const ProductsState._();
 
-final class ProductsInitial extends ProductsState {}
+  @override
+  List<Object> get props => [];
+}
+
+// final class ProductsInitial extends ProductsState {}
+
+// final class ProductsLoading extends ProductsState {}
+
+// final class ProductsSuccess extends ProductsState {}
+
+// final class ProductsFailure extends ProductsState {}
