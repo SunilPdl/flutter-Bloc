3462882d7d630b1c03be62f06c1cbf4ba08ea3b5
SunilPdl <mrsunilpaudel@gmail.com>
2024-01-16 14:41:16 +0545

    update : directory name

--- first_flutter_bloc/lib/bloc/products_bloc.dart
+++ first_flutter_bloc/lib/bloc/products_bloc.dart
@@ -1,4 +1,6 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:first_flutter_bloc/models/product.dart';
 import 'package:meta/meta.dart';
 
 part 'products_event.dart';
