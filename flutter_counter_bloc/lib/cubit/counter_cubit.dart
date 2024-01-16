c80ef4d0879f38cb776c0cf39d5aed3fc424ed55
SunilPdl <mrsunilpaudel@gmail.com>
2024-01-16 13:56:27 +0545

    add : increment , decrement function

--- flutter_counter_bloc/lib/cubit/counter_cubit.dart
+++ flutter_counter_bloc/lib/cubit/counter_cubit.dart
@@ -1,23 +1,17 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
-part 'counter_state.dart';
 
-class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterInitial());
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
 
   void incrementCount() {
-    final count = state.count + 1;
-    final updateState = CounterInitial(count: count);
-    emit(updateState);
+    emit(state + 1);
   }
 
   void decrementCount() {
-    if (state.count == 0) {
+    if (state == 0) {
       return;
     }
-    final count = state.count - 1;
-    final updateState = CounterInitial(count: count);
-    emit(updateState);
+    emit(state - 1);
   }
 }
