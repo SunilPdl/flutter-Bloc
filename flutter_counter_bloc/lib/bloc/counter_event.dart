f20c7b63bbca48a4ad82120a7ff685fc9f76e82a
SunilPdl <mrsunilpaudel@gmail.com>
2024-01-17 11:35:58 +0545

    add : counter event

--- flutter_counter_bloc/lib/bloc/counter_event.dart
+++ flutter_counter_bloc/lib/bloc/counter_event.dart
@@ -2,3 +2,7 @@ part of 'counter_bloc.dart';
 
 @immutable
 sealed class CounterEvent {}
+
+final class CounterIncrementPressed extends CounterEvent {}
+
+final class CounterDecrementPressed extends CounterEvent {}
